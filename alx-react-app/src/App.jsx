4519a0a65a997e5d614d4db8b158d6742b132437
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-07-13 22:42:56 +0300

    add files in components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -29,8 +33,18 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div>
+        <Header />
+      </div>
+      <div>
+        <MainContent />
+      </div>
+      <div>
         <WelcomeMessage />
       </div>
+      <div>
+        <Footer />
+      </div>
+
     </>
   )
 }
