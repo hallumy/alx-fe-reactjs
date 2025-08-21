4473751c28b3899710aa9b20546abb98cda4d173
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-21 18:26:31 +0300

    Add components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MainContent() {
   return (
     <main>
-        <p stle={{ color: 'white'}}>I love to visit New York, Paris, and Tokyo.</p>
+        <p style={{ color: 'white'}}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
