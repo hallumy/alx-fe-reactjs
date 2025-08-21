1d1d100e7afa60292da822d58cd223875ff751fc
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-21 18:24:30 +0300

    Add components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+     <footer style={{ color: blue, fontsize: 'small'}}>
+         <p>© 2023 City Lovers</p>
+     </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+        <p stle={{ color: 'white'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
