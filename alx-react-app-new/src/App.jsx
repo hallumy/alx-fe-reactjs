f11b3c2eb12ea8b220bd03f8e035ae8f55593ba8
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-21 18:41:21 +0300

    Add a counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -45,6 +46,10 @@ function App() {
       <div>
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />      </div>
       <div>
+      <div className="App">
+        <h1>React Counter App</h1>
+        <Counter />
+      </div>
         <Footer />
       </div>
 
