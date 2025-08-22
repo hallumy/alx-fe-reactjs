56d4b72af6ee021e7592da469026dfa3f2799565
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-08-22 08:17:36 +0300

    Add my-company

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return 
       <div>
         <nav style={('backgroundColor: blue, padding: 16px')}>
-            <ul style={('display: "flex", listStyle: none, gap: 16px, margin: 0, padding: 0 ')} >
+            <ul style={('display: "flex", justifyContent:space-between, listStyle: none, gap: 16px, margin: 0, padding: 0 ')} >
                 <li><Link to ="/" style={('color:white, textDecoration: None')}>Home</Link> </li>
                 <li><Link to="/about" style={('color:white, textDecorator: None')}>About</Link></li>
                 <li><Link to ="/contact" style={('color:white, textDecoration: None')}>Contact</Link> </li>
